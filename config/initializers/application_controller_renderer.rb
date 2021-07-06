533e3095fbce097baeb07e23b3323a2b456406ea
akachiryo <rirugameri@icloud.com>
2021-07-06 12:00:17 +0000

    finish 210706

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = "8ecbc0d2aa439bf6459b61e51fefff1753792b74fbccb76b152258d546cd787cd9d1ab4750b24e53657e8d4482fcc94dc76448863262e2e6fbe2986c848388eb"
